55c05b727cd81a01312dcefa21a7a43d1ce8e30f
robalex01 <robinballet968@gmail.com>
2026-08-21 20:50:54 +0200

    mise a jour api

--- api/staff-action.js
+++ api/staff-action.js
@@ -33,6 +33,7 @@ export default async function handler(req, res) {
 
     return res.status(400).json({ success: false, message: 'Action inconnue' });
   } catch (e) {
+    console.error('staff-action error:', e);
     return res.status(500).json({ success: false, message: e.message });
   }
 }
